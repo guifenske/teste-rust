8d265c889997f50af1c805d665e531d52b9fefbc
Guilherme Melo <guifenske@gmail.com>
2026-08-31 00:10:10 -0300

    Começo de uma calculadora

---
+++ src/calc_utils.rs
@@ -0,0 +1,98 @@
+use std::io;
+
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+}
+
+struct Calculation {
+    op: Operation,
+    a: f32,
+    b: f32,
+}
+
+fn get_calculation(op: &str, a: &str, b: &str) -> Calculation {
+    let op = op.trim();
+    let a = a.trim().parse().unwrap();
+    let b = b.trim().parse().unwrap();
+    let op = match op {
+        "+" => Operation::Add,
+        "-" => Operation::Sub,
+        "*" => Operation::Mul,
+        "/" => Operation::Div,
+        "^" => Operation::Pow,
+        _ => panic!("Operação inválida"),
+    };
+    Calculation { op, a, b }
+}
+
+fn calculate(calc: &Calculation) -> f32 {
+    match calc.op {
+        Operation::Add => {
+            let result = calc.a + calc.b;
+            result
+        }
+        Operation::Sub => {
+            let result = calc.a - calc.b;
+            result
+        }
+        Operation::Mul => {
+            let result = calc.a * calc.b;
+            result
+        }
+        Operation::Div => {
+            let result = calc.a / calc.b;
+            result
+        }
+        Operation::Pow => {
+            let result = calc.a.powf(calc.b);
+            result
+        }
+    }
+}
+
+pub fn init() {
+    let mut result: f32 = 0.0;
+    let mut should_continue = String::new();
+    println!("Calculadora iniciou!");
+
+    loop {
+        let mut a = String::new();
+        let mut b = String::new();
+        let mut op = String::new();
+        let calc: Calculation;
+        println!("Informe a operação (+, -, *, /):");
+        io::stdin().read_line(&mut op).unwrap();
+
+        if should_continue.trim() == "s" {
+            println!("Informe o número:");
+            io::stdin().read_line(&mut a).unwrap();
+
+            calc = get_calculation(&op, &mut result.to_string(), &a);
+            result = calculate(&calc);
+        } else {
+            println!("Informe o primeiro número:");
+            io::stdin().read_line(&mut a).unwrap();
+
+            println!("Informe o segundo número:");
+            io::stdin().read_line(&mut b).unwrap();
+
+            calc = get_calculation(&op, &a, &b);
+            result = calculate(&calc);
+        }
+
+        should_continue = String::new();
+
+        println!("Resultado da conta: {}", result);
+        println!("Deseja realizar outra conta?");
+
+        io::stdin().read_line(&mut should_continue).unwrap();
+
+        if should_continue.trim() != "s" {
+            break;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,7 @@
-use std::collections::HashMap;
-
-enum LoginStatus {
-    Success(String),
-    //Error(String),
-}
+mod calc_utils;
 
 fn main() {
-    let mut nome = String::from("Guilherme");
-    let nome1 = &mut nome;
-
-    nome1.push_str(" Melo");
-
-    println!("{}", nome1);
-
-    let frutas = ["Pera", "Banana", "Laranja"];
-
-    println!("Ultima fruta: {}", frutas[2]);
-
-    let mut frutas2 = vec!["Pera", "Uva"];
-    frutas2.push("Banana");
-
-    println!("Ultima fruta vetor: {}", frutas2[frutas2.len() - 1]);
-
-    //Tuple
-    let pessoa = get_pessoa();
-
-    println!("Nome: {}", pessoa.0);
-    println!("Idade: {}", pessoa.1);
-    println!("Ativa: {}", pessoa.2);
-
-    //HashMap
-    let mut cidade_capital = HashMap::new();
-    cidade_capital.insert("Rio Grande do Sul", "Porto Alegre");
-    cidade_capital.insert("Distrito Federal", "Brasilia");
-
-    if let Some(cidade) = cidade_capital.get("Distrito Federal") {
-        println!("Capitadal do Distrito Federal: {}", cidade);
-    }
-
-    let frutas3 = ["Pera", "Abacaxi", "Laranja"];
-
-    for fruta in frutas3 {
-        println!("Fruta: {}", fruta);
-
-        println!("Teste")
-    }
-
-    //ou
-
-    println!("{:?}", frutas3);
-
-    frutas2.pop(); //frutas2.remove(index);
-
-    for fruta in &frutas2 {
-        println!("Eu gosto de: {}", fruta);
-    }
-
-    println!("{:?}", frutas2);
-
-    for (estado, cidade) in &cidade_capital {
-        println!("Capital do {} e {}", estado, cidade);
-    }
-
-    print!("{:?}", cidade_capital);
-
-    struct Cidadao {
-        nome: String,
-        idade: u32, //sempre sera positivo
-    }
-
-    let mut guilherme = Cidadao {
-        nome: String::from("Guilherme Melo"),
-        idade: 18,
-    };
-
-    guilherme.idade = 19;
-
-    println!("Nome: {}", guilherme.nome);
-    println!("Idade: {}", guilherme.idade);
-
-    //enum
-
-    let result1 = LoginStatus::Success(String::from("Welcome, {name}!"));
-
-    match result1 {
-        LoginStatus::Success(message) => println!("Success: {}", message),
-        //LoginStatus::Error(message) => println!("Error: {}", message),
-    }
+    calc_utils::init();
 }
 
 /*
@@ -94,18 +9,12 @@ fn teste(name: &str) {
     println!("Hello {}!", name);
 }
 
-fn soma(a: i32, b: i32) -> i32 {
-    a + b
-}
 
-fn soma(a: i32, b: i32) -> i32 {
-    return a + b;
-}
-*/
 
 fn get_pessoa() -> (String, i32, bool) {
     (String::from("Guilherme"), 19, true)
 }
+*/
 
 //unsigned int nao permite valores negativos
 //o signed int permite valores negativos, e o bit mais importante
